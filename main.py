5bbf7c537f1129d168a19d55d50e2d4e271a9096
Ataxanov <asherzod2197@gmail.com>
2026-04-23 20:37:23 +0500

    Add UserLogin class for user authentication

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+class UserLogin:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
+    def login(self, input_password):
+        if self.password == input_password:
+            print("Xush kelibsiz")
+        else:
+            print("Parol xato")
+
+
+# Obyekt
+user = UserLogin("ali", "1234")
+user.login("1234")
+user.login("0000")
